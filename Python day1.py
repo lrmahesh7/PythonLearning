4a804cfdb0a1b952c72e0a5cd9ba077cfcd094a0
maheshbabu <lrmahesh7@gmail.com>
2019-07-21 20:39:30 -0700

    fist day practice

---
+++ Python day1.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys 
+
+
+# In[6]:
+
+
+print("hello")
+
+
+# In[10]:
+
+
+x =10
+y=20
+print(x+y)
+
+
+# In[19]:
+
+
+name = "mahes hbabu"
+
+
+# In[17]:
+
+
+print(name.upper())
+
+
+# In[18]:
+
+
+print(name.title())
+
+
+# In[ ]:
+
+
+name="mahesh babu"
+
+
+# In[ ]:
+
+
+#title is used to gives you a formatted to present end user in better way programmer no 
+#need to worry about formatted string
+
+
+# In[21]:
+
+
+print(name.title())
+
+
+# In[22]:
+
+
+print(name.lower())
+
+
+# In[24]:
+
+
+print(name.capitalize())
+
+
+# In[ ]:
+
+
+#f string introduced in 3.6 python
+
+
+# 
+
+# In[38]:
+
+
+first_name="lr"
+last_name=" mahesh babu"
+name =f"{first_name}{last_name}"
+print(name.title())
+
+
+# In[ ]:
+
+
+#adding tabs and white spaces
+
+
+# In[39]:
+
+
+print("mahesh ")
+
+
+# In[40]:
+
+
+print('\t mahesh')
+
+
+# In[58]:
+
+
+print('langaguage:\nc\nc++\njava\nscala\npython')
+
+
+# In[51]:
+
+
+str="           mahesh is a very bad boy   sdfaskdfkasd"
+print(str.lstrip())
+
+
+# In[59]:
+
+
+#single qoute,double qoute,thriple qoute
+
+name="mahesh"
+college='sbvv'
+branch="""mca"""
+print(name,college,branch)
+
+
+# In[52]:
+
+
+str=" mahesh is a very bad boy   sdfaskdfkasd   "
+print(str.rstrip())
+
+
+# In[55]:
+
+
+str=" mahesh    is a               very bad boy   sdfaskdfkasd   "
+print(str.strip())
+
+
+# In[ ]:
+
+
+
+
